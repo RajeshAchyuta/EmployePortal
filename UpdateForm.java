9954497b02c230462264b4a3cb18b4aa2ddb4a8a
Rajesh_Achyuta <rajeshachyuta1947@gmail.com>
2026-05-09 11:43:02 +0530

    Update UpdateForm.java

--- UpdateForm.java
+++ UpdateForm.java
@@ -35,7 +35,7 @@ public class UpdateForm extends HttpServlet {
 			Connection con = DriverManager.getConnection(
 					"jdbc:mysql://localhost:3306/jdbc",
 					"root",
-					"Rajesh@1947");
+					"Yourpassword");
 
 			PreparedStatement pst = con.prepareStatement(
 					"select * from employee where Emp_Id=?");
